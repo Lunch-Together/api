66d661bddf6c8cc33a1250f61f66f1e1ed29f8bb
GwonHyeok <me@ghyeok.io>
2018-11-11 20:57:39 +0900

    api error handling 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const errorHandler = require('api-error-handler');
 
 const app = express();
 
@@ -29,14 +30,6 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(errorHandler());
 
 module.exports = app;
